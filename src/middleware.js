ec5cf09ab3f45b2676df9f30f6eeacc161fe0dbf
Itzy Sabo <isabo@users.noreply.github.com>
2020-09-02 21:28:36 +0300

    Document the way middleware is combined

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,12 @@
+/**
+ * Returns a Hyperapp middleware function (wrapper for dispatch) that combines
+ * two other middleware functions. The outer function will be the first to be
+ * called by Hyperapp, so it has the first opportunity to make changes.
+ *
+ * @param {function} outer
+ * @param {function} inner
+ * @returns {function}
+ */
 export function combineMiddleware(outer, inner) {
   return function middleware(dispatch) {
     return outer(inner(dispatch));
